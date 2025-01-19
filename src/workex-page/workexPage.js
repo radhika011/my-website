996dcd936607e3705db2832f7de344583c4bc474
Radhika Sunil Wadekar <rsw4@illinois.edu>
2025-01-19 15:12:10 -0600

    completed work ex

--- src/workex-page/workexPage.js
+++ src/workex-page/workexPage.js
@@ -6,7 +6,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
+import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
@@ -14,7 +14,9 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-
+import List from '@mui/material/List';
+import InboxIcon from '@mui/icons-material/Inbox';
+import KeyboardArrowRightRoundedIcon from '@mui/icons-material/KeyboardArrowRightRounded';
 import '../App.css';
 import * as motion from "motion/react-client";
 import Timeline from '@mui/lab/Timeline';
@@ -24,10 +26,9 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
+
 import LaptopMacIcon from '@mui/icons-material/LaptopMac';
-import HotelIcon from '@mui/icons-material/Hotel';
-import RepeatIcon from '@mui/icons-material/Repeat';
+
 
 
 export default function WorkExPage() {
@@ -55,23 +56,45 @@ export default function WorkExPage() {
           variant="body2"
           color="text.secondary"
         >
-          October 2024 - Present
+        <p>
+        October 2024 - Present <br/>
+         Champaign, IL
+        </p>
+
         </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector />
           <TimelineDot>
-            <FastfoodIcon />
+           <LaptopMacIcon />
           </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent sx={{ py: '12px', px: 2 }}>
-          <Typography variant="h6" component="span">
+          <Typography variant="h5" component="span">
             University of Illinois, Urbana - Champaign
           </Typography>
-          <Typography>Improving the user interface of an AI diagnostic tool by refactoring plain HTML/CSS/JavaScript into ReactJS components &
-                      implementing effective state management
-                      ●​ Expanding SpringBoot API and MySQL database to take additional features around the level of emergency and locale codes to
-                      serve the application in other languages and streamlining the ML pipeline to increase efficiency</Typography>
+
+          <Typography>
+          <List>
+          <Typography variant="h6" component="span">
+                                                    Full-Stack Software Engineer
+                                                  </Typography>
+          <ListItem>
+           <ListItemIcon>
+                          <KeyboardArrowRightRoundedIcon/>
+                        </ListItemIcon>
+          Improving the user interface of an AI diagnostic tool by refactoring plain HTML/CSS/JavaScript into ReactJS components &
+                      implementing effective state management</ListItem>
+                      <ListItem>
+                       <ListItemIcon>
+                                                <KeyboardArrowRightRoundedIcon/>
+                                              </ListItemIcon>
+             Expanding SpringBoot API and MySQL database to take additional features around the level of emergency and locale codes to
+                      serve the application in other languages and streamlining the ML pipeline to increase efficiency</ListItem>
+
+                      </List>
+
+                      </Typography>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
@@ -80,7 +103,10 @@ export default function WorkExPage() {
           variant="body2"
           color="text.secondary"
         >
-          10:00 am
+        <p>
+          July 2023 - June 2024 <br/>
+          Pune, India
+          </p>
         </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector />
@@ -90,42 +116,102 @@ export default function WorkExPage() {
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent sx={{ py: '12px', px: 2 }}>
-          <Typography variant="h6" component="span">
-            Code
+          <Typography variant="h5" component="span">
+            Citigroup
           </Typography>
-          <Typography>Because it&apos;s awesome!</Typography>
+
+          <Typography>
+          <Typography variant="h6" component="span">
+                               Software Development Engineer - 1
+                              </Typography>
+           <List>
+                    <ListItem>
+                     <ListItemIcon>
+                                    <KeyboardArrowRightRoundedIcon/>
+                                  </ListItemIcon>
+                   Developed 20+ consumer-ready REST API endpoints using Flask to achieve summarization of SonarQube reports across
+                   500+ organization-wide projects reducing the time required for code quality report reviews from 60 to 3 minutes per report</ListItem>
+                                <ListItem>
+                                 <ListItemIcon>
+                                                          <KeyboardArrowRightRoundedIcon/>
+                                                        </ListItemIcon>
+                      Designed and implemented a user-friendly ReactJS frontend using custom components including a live dashboard to
+                      visualize key metrics like code coverage, technical debt, and the number of issues, resulting in a 50% decrease in developer
+                      support requests received by the DevOps team</ListItem>
+                     <ListItem>
+                                                     <ListItemIcon>
+                                                                              <KeyboardArrowRightRoundedIcon/>
+                                                                            </ListItemIcon>
+                                         Led the integration of MongoDB with the Flask API to manage and store user preferences using PyMongo for data retrieval</ListItem>
+
+
+
+                                </List>
+
+
+
+          </Typography>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
+       <TimelineOppositeContent
+                sx={{ m: 'auto 0' }}
+                align="right"
+                variant="body2"
+                color="text.secondary"
+              >
+              <p>
+                May 2022 - July 2022<br/>
+                Pune, India
+                </p>
+              </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector />
           <TimelineDot color="primary" variant="outlined">
-            <HotelIcon />
+           <LaptopMacIcon />
           </TimelineDot>
           <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
         </TimelineSeparator>
-        <TimelineContent sx={{ py: '12px', px: 2 }}>
-          <Typography variant="h6" component="span">
-            Sleep
-          </Typography>
-          <Typography>Because you need rest</Typography>
-        </TimelineContent>
+    <TimelineContent sx={{ py: '12px', px: 2 }}>
+             <Typography variant="h5" component="span">
+               Citigroup
+             </Typography>
+
+             <Typography>
+             <Typography variant="h6" component="span">
+                                   Software Development Enginner Intern
+                                 </Typography>
+              <List>
+                       <ListItem>
+                        <ListItemIcon>
+                                       <KeyboardArrowRightRoundedIcon/>
+                                     </ListItemIcon>
+                    Pioneered an object-oriented, reusable KYC data quality validation framework leveraging PySpark using custom validation
+                    rules tailored to specific data requirements. This framework reduced the time required for row and column validation of
+                    100K+ records occurring before migration from OLTP (Oracle DB) to OLAP (Hive) by 92%</ListItem>
+                                   <ListItem>
+                                    <ListItemIcon>
+                                                             <KeyboardArrowRightRoundedIcon/>
+                                                           </ListItemIcon>
+                       Engineered a shell script to automate the periodic triggering of the framework module on a weekly basis, enhancing process
+                       automation and streamlining the validation process</ListItem>
+                        <ListItem>
+                                                        <ListItemIcon>
+                                                                                 <KeyboardArrowRightRoundedIcon/>
+                                                                               </ListItemIcon>
+                                           Formulated an HTML email reporting framework in Python to report the results of validation in a user-friendly format to
+                                           developers, database administrators and project managers</ListItem>
+
+
+
+                                   </List>
+
+
+
+             </Typography>
+           </TimelineContent>
       </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-          <TimelineDot color="secondary">
-            <RepeatIcon />
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent sx={{ py: '12px', px: 2 }}>
-          <Typography variant="h6" component="span">
-            Repeat
-          </Typography>
-          <Typography>Because this is the life you love!</Typography>
-        </TimelineContent>
-      </TimelineItem>
+
     </Timeline>
     </Box>
                   </motion.div>
